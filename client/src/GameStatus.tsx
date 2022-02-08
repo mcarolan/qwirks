3ef4a77ac0a153b619ed33ab9d325123846ab7df
Martin Carolan <mail@mcarolan.net>
2022-02-08 21:33:05 +0000

    Better status

--- client/src/GameStatus.tsx
+++ client/src/GameStatus.tsx
@@ -8,19 +8,16 @@ export interface GameStatusProps {
 }
 
 export function GameStatus(props: GameStatusProps) {
-  let status = "Invite 2-4 players and press start!";
+  let status = "";
 
-  if (props.userIsInControl) {
+  if (!props.isStarted) {
+    status = "Invite 2-4 players and press start!";
+  } else if (props.winningUsername) {
+    status = `Well done ${props.winningUsername}`;
+  } else if (props.userIsInControl) {
     status = "It's your go!";
-  }
-
-  if (props.waitingForUsername) {
+  } else if (props.waitingForUsername) {
     status = `Waiting for ${props.waitingForUsername}...`;
   }
-
-  if (props.winningUsername) {
-    status = `Well done ${props.winningUsername}`;
-  }
-
   return <div className="gameStatus">{status}</div>;
 }
